bdf228150474cd2f30df49d2d7a62f9a8ad3c1e7
Mateusridu <mateusridu@gmail.com>
2026-04-08 15:33:49 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.scss'
+import { Routes, Route, BrowserRouter, HashRouter } from 'react-router-dom'
+import Layout from './components/layout'
+import Home from './components/home'
+import About from './components/about'
+import Contact from './components/contact'
+
+function App() {
+    return (
+        <HashRouter basename={import.meta.env.DEV ? '/' : '/paula-portfolio/'}>
+            <Routes path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route index path="about" element={<About />} />
+                <Route index path="contact" element={<Contact />} />
+            </Routes>
+        </HashRouter>
+    )
+  
+}
+
+export default App;
