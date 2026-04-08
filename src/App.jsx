9568bae2454633ed591fc4f862a9f27ce3f8279f
Mateusridu <mateusridu@gmail.com>
2026-04-08 18:45:48 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.scss';
+import { Routes, Route, HashRouter } from 'react-router-dom'; // Remove BrowserRouter import
+import Layout from './components/layout';
+import Home from './components/home';
+import About from './components/about';
+import Contact from './components/contact';
+
+function App() {
+    return (
+        // Use HashRouter with a fixed basename for GitHub Pages deployment
+        <HashRouter basename="/paula-portfolio/"> 
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} /> {/* Home route */}
+                    <Route path="about" element={<About />} /> {/* About route */}
+                    <Route path="contact" element={<Contact />} /> {/* Contact route */}
+                </Route>
+            </Routes>
+        </HashRouter>
+    );
+}
+
+export default App;
