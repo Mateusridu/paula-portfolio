461b892a1ea2200a292a77111aa198631012962c
Mateusridu <mateusridu@gmail.com>
2026-05-13 14:56:44 -0300

    Add files via upload

---
+++ src/components/home/logo/index.jsx
@@ -0,0 +1,40 @@
+import './index.scss'
+import LogoS from '../../../assets/images/logo-s.png'
+import { useEffect, useRef } from 'react'
+import { gsap } from "gsap"
+
+
+const Logo = () => {
+    const bgRef = useRef();
+    const solidLogoRef = useRef();
+
+    useEffect(()=> {
+        
+
+        gsap.timeline()
+        .to(bgRef.current, {
+            duration: 1,
+            opacity: 10,
+        })
+
+        gsap.fromTo(
+            solidLogoRef.current,
+            {
+                opacity: 0,
+            },
+            {
+                opacity: 1,
+                delay: 4,
+                duration: 4,
+            }
+        )
+    },[])
+
+    /*return (
+        <div className="logo-container" ref={bgRef}>
+            <img ref={solidLogoRef} className="solid-logo" src={LogoS} alt="S" />
+        </div> 
+    )*/
+}
+
+export default Logo
